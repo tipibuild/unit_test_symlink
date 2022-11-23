86e36df3fa922aff3658cbf4faf97a2395466bd4
Lambourl <lmbrluc@gmail.com>
2022-11-23 08:21:05 +0100

    :wink: add all file for test

---
+++ cherry.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"I like cherry"<<std::endl;
+}
---
+++ repodir/subdir/strawberry.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"I like strawberry"<<std::endl;
+}
